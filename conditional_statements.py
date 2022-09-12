91e5350e814ea54c5453b096c9a7e11bc5edc21a
abhisekthapam <ahisekmagar250203@gmail.com>
2022-09-12 10:02:30 +0545

    if elif else

---
+++ conditional_statements.py
@@ -0,0 +1,11 @@
+age = int(input("Please provide your age : "))
+
+ # conditional statement
+ # if block only
+
+print("Code started...")
+if age > 18: 
+    print(f"You can play this because you are above {age}")
+else:
+    print("You are too young for this program.")
+print("Code ended...")
---
+++ movie_ticket.py
@@ -0,0 +1,8 @@
+# proper use of if-elif-else block
+age = int(input("Please provide your age : "))
+if age <=0:
+    print("Please input your valid date")
+if 0<age<10:
+    print("So you are kid, Your movie ticket is Nrs.100")
+elif 10<age<20:
+    print("Your movie ticket is 250")
