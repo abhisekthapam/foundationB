acedace93f922d2a290d6fa8630dd12e31a59b71
abhisekthapam <ahisekmagar250203@gmail.com>
2022-09-14 10:06:01 +0545

    Changes

--- Looping_statement2.py
+++ Looping_statement2.py
@@ -6,8 +6,8 @@ Brand_Name MOdel_Name available_at price
 
 Laptop = input("Which laptop do you want to buy? ")
 if Laptop == "dell":
- print('''Your laptop brand name is Dell
-Your Model number is Dell G15 5520
+ print('''Your laptop brand name is Dell.
+Your Model number is Dell G15 5520.
 Available price is 1,85,000. ''')
 else :
     print("We have only dell brand laptop right now.")
