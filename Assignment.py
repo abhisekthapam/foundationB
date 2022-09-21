4b60800144944a004320ba423608008262417791
abhisekthapam <ahisekmagar250203@gmail.com>
2022-09-21 07:58:31 +0545

    assignment

--- Assignment.py
+++ Assignment.py
@@ -45,13 +45,18 @@ else :
         
         
 #4
-num_list = []   
-for num in range(1,8):
-    num_list.append(num)
-    week_days_list = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
-for item in week_days_list : 
-        print(f"The day {num} is :--> {item}")
-        
+week_days_list = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"]
+counter = 0
+for day in week_days_list:
+    counter += 1
+    print(f"The day {counter} is :--> {day} ")
         
         
 
---
+++ hww.py
@@ -0,0 +1,7 @@
+
+num_list = []   
+for num in range(1,8):
+ num_list.append(num)
+ week_days_list = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+ for item in week_days_list : 
+   print(f"The day {num} is :--> {item}")
